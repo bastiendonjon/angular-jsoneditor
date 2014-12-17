ff196a05c9a03fa4484be3e0c1e055a6e66a804c
Bastien Donjon <bastien.donjon@concoursmania.com>
2014-12-17 17:38:50 +0100

    Add files

---
+++ angular-jsoneditor.js
@@ -0,0 +1,47 @@
+/*
+ * angular-jsoneditor 1.0.0
+ * (c) 2014 Bastien Donjon http://bastien-donjon.fr
+ * License: MIT
+ */
+
+(function(window, angular) {
+
+  'use strict';
+
+  angular.module('ngJsonEditor', [])
+    .directive('jsoneditor', directive);
+
+  /**
+   * @returns {{scope: {}, restrict: string, transclude: boolean, link: link}}
+   */
+  function directive() {
+
+    function link(scope, element, attrs) {
+      var editor,
+        defaultOptions = {
+          mode: 'form',
+          editable: false
+        },
+        options = angular.extend(defaultOptions, scope.options);
+
+      editor = new JSONEditor(element[0], options);
+      scope.$watch(
+        'json',
+        function (newValue, oldValue) {
+          editor.set(newValue);
+        }
+      );
+    }
+
+    return {
+      scope: {
+        options: '=',
+        json: '='
+      },
+      restrict: 'E',
+      transclude: false,
+      link: link
+    };
+  };
+
+})(window, angular);
